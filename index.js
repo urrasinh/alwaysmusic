f2df69a70c39a8b5ce334486e58190d685cf0091
urrasinh <46355836+urrasinh@users.noreply.github.com>
2022-05-25 22:50:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const process = require('process');
-const { consulta, nuevo, editar, rut } = require('./consultas');
-
 const argumentos = process.argv
 
-const accionSql = argumentos[2]
-const param1 = argumentos[3] 
+const { rut, consulta, nuevo, editar, eliminar } = require('./consultas')
+
+const acccionSql = argumentos[2] 
+const param1 = argumentos[3]
 const param2 = argumentos[4]
 const param3 = argumentos[5]
 const param4 = argumentos[6]
 
-
-switch (accionSql) {
-    case 'consulta':
-    consulta()
-    break
+switch (acccionSql) {
     case 'nuevo':
-    nuevo(param1, param2, param3, param4)
-    break
+        nuevo(param1, param2, param3, param4)
+        break
+    case 'consulta':
+        consulta()
+        break
     case 'editar':
-    editar(param1, param2, param3, param4)
+        editar(param1, param2, param3, param4)
         break
     case 'rut':
-    rut(param1)
-    break
+        rut(param1)
+        break
     case 'eliminar':
-    eliminar(param1)
-    break
+        eliminar(param1)
+        break
     default:
-        console.log('error')
+        console.log('Error: funcion no especificada o mal escrita.')
         break
 }
-
